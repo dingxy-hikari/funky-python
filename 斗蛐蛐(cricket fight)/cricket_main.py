50fe8ed762613996ecb45ab838f6d9023d560f05
dingxy-hikari <dingx9320@gmail.com>
2023-03-05 22:04:00 +0800

    2022.03.05

---
+++ 斗蛐蛐(cricket fight)/cricket_main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mb
+
+
+
+a = 'aua'
+
+
+class cricket_jar:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    print(a)
