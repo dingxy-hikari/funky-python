d8515402a10890f6b822d4637f65ce9257642816
dingxy-hikari <dingx9320@gmail.com>
2023-03-09 23:25:33 +0800

    2022.03.08

--- 斗蛐蛐(cricket fight)/cricket_list_Manager.py
+++ 斗蛐蛐(cricket fight)/cricket_list_Manager.py
@@ -1,13 +1,51 @@
 import json
 import os
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mgb
 
 with open('cricket_list.json', 'r') as cfp:
     cricket_list = json.load(cfp)
 
 
-
 class cricket_importer:
     def __init__(self):
+        self.error_type = 0
+        self.name = None
+        self.hp = None
+        self.atk = None
+        self.dfn = None
+        self.root = tk.Tk()
+        self.title = tk.Label()
+        self.frm1 = tk.Frame(self.root)
+        self.label1 = tk.Label(self.frm1)
+        self.entry1 = tk.Entry(self.frm1)
+        self.label2 = tk.Label(self.frm1)
+        self.entry2 = tk.Entry(self.frm1)
+        self.label3 = tk.Label(self.frm1)
+        self.entry3 = tk.Entry(self.frm1)
+        self.label4 = tk.Label(self.frm1)
+        self.entry4 = tk.Entry(self.frm1)
+        self.button = ttk.Button(self.root, command=lambda: self.information_input())
+        self.title.pack()
+        self.label1.grid(row=0, column=0)
+        self.entry1.grid(row=0, column=1)
+        self.label2.grid(row=1, column=0)
+        self.entry2.grid(row=1, column=1)
+        self.label3.grid(row=2, column=0)
+        self.entry3.grid(row=2, column=1)
+        self.label4.grid(row=3, column=0)
+        self.entry4.grid(row=3, column=1)
+        self.frm1.pack()
+        self.button.pack()
+
+    def is_int(self):
+        try:
+            pass
+        except ValueError:
+            self.error_type = 2
+
+    def information_input(self):
         pass
 
     def main(self):
