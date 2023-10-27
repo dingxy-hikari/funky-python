e825f380c680ea4ac3cb49fdf2d328927989e5ea
dingxy-hikari <dingx9320@gmail.com>
2023-10-27 10:02:38 +0800

    2024 respawn update

---
+++ 1a58女友/1a58女友.py
@@ -0,0 +1,22 @@
+import time
+import os
+
+print('请为您的AI女朋友命名!')
+AI_name = input()
+os.system('cls')
+print('请您与AI女友对话：')
+text = input()
+os.system('cls')
+print('您：')
+print(text)
+time.sleep(3)
+os.system('cls')
+print('您的AI女友正在回复，清稍等……')
+time.sleep(3)
+os.system('cls')
+print((AI_name+':'))
+print('抱歉，因为您刚才说的话，我认为我们不合适，分手吧')
+os.system('pause')
+os.system('cls')
+print('你被甩了！')
+os.system('pause')
